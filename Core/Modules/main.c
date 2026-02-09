8fb9f9319c6418ceb6000275f9b6a97e204065da
konLiogka <ntinaliogka@gmail.com>
2026-02-09 21:25:04 +0200

    [Startup] Separate startup animation from main
    v0.6.6

--- Core/Modules/main.c
+++ Core/Modules/main.c
@@ -1,44 +1,7 @@
 #include <main.h>
-#include "display.hpp"
-
+#include "startup_animation.hpp"
 
 uint8_t err_code = 0;
-
-void showStartupScreen()
-{
-  const uint8_t steps[] = {16, 8, 4, 2, 1};
-
-  for (uint8_t step : steps)
-  {
-    Display::clear();
-
-    for (uint8_t y = 0; y < 8; y += (step > 8 ? 1 : step))
-    {
-      for (uint8_t x = 0; x < 128; x += step)
-      {
-        uint8_t blockX = x + (step / 2);
-        uint8_t blockY = y + (step > 8 ? 4 : step / 2);
-
-        if (startup_screen_bitmap.data[blockY * 128 + blockX] & 0x01)
-        {
-          for (uint8_t by = y; by < y + (step > 8 ? 1 : step) && by < 8; by++)
-          {
-            Display::setCursor(x, by);
-            uint8_t pixels = 0xFF;
-            for (uint8_t bx = 0; bx < step && (x + bx) < 128; bx++)
-            {
-              Display::writeData(&pixels, 1);
-            }
-          }
-        }
-      }
-    }
-
-    HAL_Delay(150);
-  }
-
-  Display::drawBitmap(startup_screen_bitmap, 0, 0);
-}
 
 int main(void)
 {
@@ -46,10 +9,10 @@ int main(void)
   HAL_Init();
 
   Hardware::init();
+
+  Display::init();
   Display::clear();
-
-
-  showStartupScreen();
+  startupAnimation();
 
   HAL_Delay(200);
 
---
+++ Core/Startup/startup_animation.hpp
@@ -0,0 +1,6 @@
+#ifndef STARTUP_ANIMATION_HPP_
+#define STARTUP_ANIMATION_HPP_
+
+void startupAnimation(void);
+
+#endif 
